c04e79da2e025d24e5927dfb1aafe66d66011276
Eduard Donkeev <donkeev.ed@yandex.ru>
2020-10-05 00:58:12 +0300

    изменения

--- script/index.js
+++ script/index.js
@@ -2,7 +2,6 @@ const buttonOpenPopup = document.querySelector('.profile__edit-button');
 const buttonClosePopup = document.querySelector('.popup__close');
 const buttonSavePopup = document.querySelector('.popup__save-button');
 const popup = document.querySelector('.popup');
-const popupBody = document.querySelector('.popup__body')
 let  formElement = document.querySelector('.popup__form');
 let  porfileName = document.querySelector('.profile__title');
 let  profileJob = document.querySelector('.profile__subtitle');
